b7068543c8dc149c0cdd65150301ee101696081d
IshaqAlathamneh <ishaqalathamneh@gmail.com>
2021-03-28 16:25:52 +0300

    repo rename

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+// My depend..
+const express = require('express');
+const cors = require('cors');
+const PORT = 3000;
+const app = express();
+app.use(cors());
+
+app.listen(PORT, ()=> console.log(`This app is working an port ${PORT}`));
+app.get('/location', location);
+app.get('/weather', weather);
+function location() {
+    const getLocation = require('./data/location.json')
+}
